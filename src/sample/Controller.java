1456d2e63c95200085e142f5d77b26a26419c068
yairabf <yairabc@gmail.com>
2017-03-14 12:41:23 +0200

    WORKING On labels for the app

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -52,7 +52,6 @@ public class Controller {
 
             ColumnConstraints column = new ColumnConstraints(40);
             RowConstraints row = new RowConstraints(40);
-            //grid.getRowConstraints().add(row);
             grid.getColumnConstraints().add(column);
             grid.setGridLinesVisible(true);
         }
@@ -65,38 +64,10 @@ public class Controller {
         for(int i = 0; i < h; i++) {
             for(int j=0; j< w; j++) {
                 Pane pane = new Pane();
-                //pane.backgroundProperty().setValue(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-                //pane.getStyleClass().add("-fx-background-color: black, white; -fx-background-insets: 0,0,1,1,0;");
-              /*  String image1 = JavaFXBuilderFactory.class.getResource("res/rodblock.jpg").toExternalForm();
-                pane.setStyle("-fx-background-image: url('" + image1 + "'); " +
-                        "-fx-background-position: center center; " +
-                        "-fx-background-repeat: stretch;");*/
                 grid.add(pane, i, j);
-               // pane.setStyle("-fx-background-color: aqua");
-                //Image image = new Image("/res/rodblock.jpg");
-                //pane.setBackground(new Background(new BackgroundImage(image);
-
-                //table[i][j] = pane;
             }
         }
         windowPane.getChildren().add(grid);
-
-        /*double width = GridMaker.SCREEN_SIZE/w*h;
-        Pane p = new Pane();
-
-        Rectangle[][] rec = new Rectangle [h][w];
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                rec[i][j] = new Rectangle();
-                rec[i][j].setX(i * width);
-                rec[i][j].setY(j * width);
-                rec[i][j].setWidth(width);
-                rec[i][j].setHeight(width);
-                rec[i][j].setFill(null);
-                rec[i][j].setStroke(Color.BLACK);
-                p.getChildren().add(rec[i][j]);
-*/
             }
 
 }
