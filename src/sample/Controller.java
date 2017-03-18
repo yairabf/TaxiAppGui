0f730b7f0eb52a05eac69ef75d1dc5abf41a0add
maggy <yair>
2017-03-18 23:48:17 +0200

    finishing the scrollpane2.02

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,27 +1,15 @@
 package sample;
 
-import javafx.beans.binding.Bindings;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.*;
 import javafx.fxml.FXML;
-import javafx.fxml.JavaFXBuilderFactory;
-import javafx.geometry.Insets;
-import javafx.scene.CacheHint;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-
-import java.util.Random;
-
 import java.util.ArrayList;
 
 
